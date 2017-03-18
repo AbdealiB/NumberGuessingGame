304e6ccc5470625efee51b6bba4171b70a5efadc
Abdeali Bhagat <abdealibhagat787@gmail.com>
2017-03-18 21:26:54 +0530

    Adding comments

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -9,43 +9,60 @@ import random
 import sys
 
 def gamePlay():
-    y_n = input("\nDo you want to play the game? (Y/y/n/N) >>>> ")
-    if (y_n.lower() == 'y'):
-        rnd_num = random.randint(1,10)
+    y_n = input("\nDo you want to start the game? (Y/y/n/N) >>>> ")
+    if (y_n.lower() == 'y'): # If user presses y/Y and converting it into lower case
+        rnd_num = random.randint(1,10) # Generating the random number
         flag = True
         limit = 1
         for limit in range(limit,4):
             while (flag):
-                    try:
+                    try: # Checking that input from user is a number only
                         usr_int = int(input("\nGuess a number from 1-10 >>>> "))
+                        
+                        # Checking if number guessed by user is correct or not
                         if (usr_int == rnd_num):
                             print("\nYou Guessed correctly.")
+                            
+                            # How many tries it took user to guess the number, if guessed correctly
                             if (limit==1):
-                                print("\nIt took you only 1 move to Guess.")
+                                print("\nIt took you only 1 try to Guess.")
                             else:
-                                print("\nIt took you {} moves to Guess.".format(limit))
-                            flag=False
-                        elif (usr_int < rnd_num):
+                                print("\nIt took you {} tries to Guess.".format(limit))
+                            flag=False # Setting flag=false because user guessed the number correctly
+                            
+                        elif (usr_int < rnd_num): # Notifying the user that entered number is too low
                             print("\nNumber too Low.")
                             break
-                        elif (usr_int > rnd_num):
+                            
+                        elif (usr_int > rnd_num): # Notifying the user that entered number is too high
                             print("\nNumber too High.")
                             break
-                    except ValueError:
+                            
+                    # If not entered a number
+                    except ValueError: 
                         print("\nPlease enter a number only.")
                         continue
-            if (flag != False):
-                print("\nNumber of moves left >>>> {}.".format(3-limit))
-            limit+=1
+                        
+            # If number has not guessed yet
+            if (flag != False): 
+                # tries left
+                print("\nNumber of tries left >>>> {}.".format(3-limit))
+            limit+=1 # Incremeting the tries if the number is not guessed yet
+            
+        # If user was not able to guess the number in 3 tries
         if (flag != False):
             print("\nErr.. No moves left. Better Luck Next Time")
-            print("\nOriginal Number was >>>> {}.".format(rnd_num))
+            print("\nOriginal Number was >>>> {}.".format(rnd_num)) # Original Number
+            
+    # If user presses any other key
     else:
         print("\nOk, GoodBye!")
         input("\nPress ENTER to exit")
         sys.exit(1)
+
     print("\nThanks For Playing.")
     input('\nPress ENTER to exit')
+
 
 print("\n|-----------------------------------------------------------------------|")
 print("\n|\t\t\tWelcome to Number Guessing Game\t\t\t|")
